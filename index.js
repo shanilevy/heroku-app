faa2bcd3b7567a751aaf6859d308851b13cab05d
Shani Levy <shanil@jfrog.com>
2019-11-28 16:50:35 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
-process.env.DATABASE_URL='postgres://bpdkpxgtbtnxil:e3c3dcc3178b70beaea9b839e8a449bbc886e22901a994f2f756fd735f57d204@ec2-107-22-239-155.compute-1.amazonaws.com:5432/d7gpfk35g7m58s'
+process.env.DATABASE_URL='postgres://pooulhmtxyjhpb:1000f00b59392e2474b4f93a2230ab1780b4eacebfe4df2237a15a4268dcfd3c@ec2-107-22-239-155.compute-1.amazonaws.com:5432/d30edg71m0fov9'
 
 const express = require('express')
 const app = express()
@@ -17,13 +17,13 @@ app.get('/', (req, res, next) => {
        if (err) {
            console.log("Can not connect to the DB" + err);
        }
-       client.query('SELECT nickname FROM users WHERE age = (SELECT MAX(age) FROM users);SELECT name FROM salesforce.account WHERE numberofemployees = (SELECT MIN(numberofemployees) FROM salesforce.account)', function(err,result){
+       client.query('SELECT nickname FROM users WHERE age = (SELECT MAX(age) FROM users);SELECT name FROM salesforce.account WHERE numberofemployees = (SELECT MAX(numberofemployees) FROM salesforce.account)', function(err,result){
             done();
             if (err) {
                 console.log(err);
                 res.status(400).send(err);
             }
-            res.status(200).send("This is " + result[0].rows[0].nickname + "'s first Heroku Application! " + '<br/>' + result[0].rows[0].nickname + " is the Founder of the Smallest Company:  " + result[1].rows[0].name);
+            res.status(200).send("This is " + result[0].rows[0].nickname + "'s first Heroku Application! " + '<br/>' + result[0].rows[0].nickname + " is the Founder of the Biggest Company:  " + result[1].rows[0].name);
        })
    })
 });
